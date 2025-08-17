c6a13782a34f8c6d9a6be8ac2384ef0564b3dcdb
thais <thaisrodrigues2078@gmail.com>
2025-08-17 12:15:30 -0300

    Atualiza ALLOWED_HOSTS para Render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2w!p&thmu)ara4h67xlz5r-p*m45_4(kta7!4a6=0ty3%sj_=7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
