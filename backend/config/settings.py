7718114e63e4e91dfb1854390c3270e87b5b11d6
thais <thaisrodrigues2078@gmail.com>
2025-10-30 20:56:01 -0300

    mudanças nas credenciais do email

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -109,8 +109,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'thais-pacheco@estudante.rs.gov.br'  
-EMAIL_HOST_PASSWORD = 'jxgv kvie vyaj rxrd' 
+EMAIL_HOST_USER = 'capitalonline.tcc@gmail.com'  
+EMAIL_HOST_PASSWORD = 'gbhc uvov zzox xakk' 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
